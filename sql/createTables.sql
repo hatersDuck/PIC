3e957f367d05e1620938819ac8800a9b993a01cf
hatersDuck <fishlthe@gmail.com>
2023-05-07 11:00:45 +0700

    laba5 sql-createTables

---
+++ sql/createTables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE tradeStrategy (
+  strategy_id SERIAL PRIMARY KEY,
+  title CHAR(20),
+  path_file TEXT NOT NULL DEFAULT '/strategy/default.py',
+  status CHAR(6) NOT NULL DEFAULT 'off' CHECK (status IN ('active', 'pause', 'off')),
+  symbol CHAR(10) NOT NULL DEFAULT 'BTCUSDT'
+);
+
+CREATE TABLE historyData (
+  history_id SERIAL PRIMARY KEY,
+  date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+  symbol CHAR(10) NOT NULL DEFAULT 'BTCUSDT',
+  open_price DECIMAL(18,8) NOT NULL,
+  high_price DECIMAL(18,8) NOT NULL,
+  low_price DECIMAL(18,8) NOT NULL,
+  close_price DECIMAL(18,8) NOT NULL,
+  interval CHAR(2) NOT NULL DEFAULT '1m'
+);
+
+CREATE TABLE users (
+  user_id BIGSERIAL PRIMARY KEY,
+  api_key CHAR(64) NOT NULL,
+  secret_key CHAR(64) NOT NULL,
+  strategy_id SERIAL NOT NULL REFERENCES tradeStrategy(strategy_id) ON UPDATE CASCADE ON DELETE SET DEFAULT,
+  username CHAR(32) NOT NULL DEFAULT 'no'
+);
+
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  type CHAR(10) NOT NULL,
+  date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+  strategy_id SERIAL NOT NULL REFERENCES tradeStrategy(strategy_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  history_id SERIAL NOT NULL REFERENCES historyData(history_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE transactions (
+  transaction_id SERIAL PRIMARY KEY,
+  order_id SERIAL NOT NULL REFERENCES orders(order_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  user_id SERIAL NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE,
+  date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+  quantity DECIMAL(18,8) NOT NULL
+);
